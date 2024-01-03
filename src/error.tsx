32567f227d8e38a768d70e7caaab71eb2742ac35
sultan <abu-farris@hotmail.com>
2024-01-03 13:09:09 +0300

    feat: add new button to error page

--- src/error.tsx
+++ src/error.tsx
@@ -1,6 +1,6 @@
 import imgUrl from "@/assets/error.svg";
 import { Button } from "./components/ui/button";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Error = () => {
   const navigate = useNavigate();
@@ -10,14 +10,19 @@ const Error = () => {
       <div className="text-center">
         <h1 className="text-4xl md:text-8xl font-bold">404</h1>
         <p className="text-lg md:text-3xl mt-6">حدث خطأ ما!</p>
-        <Button
-          onClick={() => navigate(-1)}
-          className="mt-4"
-          variant={"default"}
-          size={"lg"}
-        >
-          العودة للصفحة السابقة
-        </Button>
+        <div className="flex flex-col">
+          <Button asChild className="mt-4" size={"lg"}>
+            <Link to="/">الصفحة الرئيسية</Link>
+          </Button>
+          <Button
+            onClick={() => navigate(-1)}
+            className="mt-4"
+            variant={"ghost"}
+            size={"lg"}
+          >
+            العودة للصفحة السابقة
+          </Button>
+        </div>
       </div>
       <img className="md:w-1/3 w-1/2" src={imgUrl} alt="logo" />
     </div>
